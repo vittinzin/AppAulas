7c2132aed8100f9fb8981d0b3b248713327a9e89
arthurx034 <1008188@senacminas.edu.br>
2025-10-01 14:26:11 -0300

    resolvendo erros no gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,8 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.7.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+}
+
+buildscript {
+    // Não precisa adicionar repositórios aqui, pois já estão no settings.gradle.kts
 }
